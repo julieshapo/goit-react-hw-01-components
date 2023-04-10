f73c4da2d1cb7939a26b0069cfe318bff8abd7e5
Julie Shapovalenko <jools.shapo@gmail.com>
2023-04-10 16:35:51 +0300

    update package json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,7 @@
+import user from '../user.json'
+import UserCard from './User'
+
+
 export const App = () => {
   return (
     <div
@@ -10,7 +14,42 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      < UserCard
+      avatar={user.avatar}
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      followers={user.stats.followers}
+      views={user.stats.views}
+      likes={user.stats.likes}
+    />
     </div>
   );
 };
+
+
+// export function App() {
+//   return <div
+//         style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//   >
+//     < UserCard
+//       avatar={data.avatar}
+//       username={data.username}
+//       tag={data.tag}
+//       location={data.location}
+//       followers={data.stats.followers}
+//       views={data.stats.views}
+//       likes={data.stats.likes}
+//     />
+//   </div>
+// }
+
+
+ 
