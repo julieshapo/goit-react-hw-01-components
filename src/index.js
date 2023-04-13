8d38dcba4d361cc990e2f2345912320b58f8d4a0
Julie Shapovalenko <jools.shapo@gmail.com>
2023-04-13 17:01:41 +0300

    install styled components

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App/App';
 import './index.css';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  colors: {
+    text: '#212121',
+    accent: 'tomato',
+  },
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
